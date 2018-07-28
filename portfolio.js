cb24011324c92f6c34297f6da2c38adca0b599d5
Taylor Pelsue <taylorpelsue@gmail.com>
2018-07-28 14:21:52 -0500

    added js game and jquery game

--- portfolio.js
+++ portfolio.js
@@ -1,4 +1,29 @@
 $(document).ready(function () {
+    // For smooth scrolling from top to portfolio
+    $("#forwardBtn").on('click', function (event) {
+
+        $('html, body').animate({
+            scrollTop: $("#navBar").offset().top
+        }, 'slow');
+
+    });
+
+    // For smooth scrolling from portfolio to hangman
+    $("#toHangman").on('click', function (event) {
+
+        $('html, body').animate({
+            scrollTop: $("#hangman").offset().top
+        }, 'slow');
+
+    });
+    // For smooth scrolling from portfolio to rpg
+    $("#toRPG").on('click', function (event) {
+
+        $('html, body').animate({
+            scrollTop: $("#rpg").offset().top
+        }, 'slow');
+
+    });
     // For smooth scrolling
     $("#forwardBtn").on('click', function (event) {
 
@@ -7,36 +32,12 @@ $(document).ready(function () {
         }, 'slow');
 
     });
-});
-// For smooth scrolling
-$("#home").on('click', function (event) {
+    // For smooth scrolling
+    $("#forwardBtn").on('click', function (event) {
 
-    $('html, body').animate({
-        scrollTop: $("#indexBanner").offset().top
-    }, 'slow');
+        $('html, body').animate({
+            scrollTop: $("#navBar").offset().top
+        }, 'slow');
 
-});
-// For smooth scrolling
-$("#forwardBtn").on('click', function (event) {
-
-    $('html, body').animate({
-        scrollTop: $("#navBar").offset().top
-    }, 'slow');
-
-});
-// For smooth scrolling
-$("#forwardBtn").on('click', function (event) {
-
-    $('html, body').animate({
-        scrollTop: $("#navBar").offset().top
-    }, 'slow');
-
-});
-// For smooth scrolling
-$("#forwardBtn").on('click', function (event) {
-
-    $('html, body').animate({
-        scrollTop: $("#navBar").offset().top
-    }, 'slow');
-
+    });
 });
