d880b26deff9fe0d1a1b5e090980b69b9c919822
Taylor Pelsue <taylorpelsue@gmail.com>
2018-07-27 21:55:10 -0500

    fixing portfolio

---
+++ portfolio.js
@@ -0,0 +1,42 @@
+$(document).ready(function () {
+    // For smooth scrolling
+    $("#forwardBtn").on('click', function (event) {
+
+        $('html, body').animate({
+            scrollTop: $("#navBar").offset().top
+        }, 'slow');
+
+    });
+});
+// For smooth scrolling
+$("#home").on('click', function (event) {
+
+    $('html, body').animate({
+        scrollTop: $("#indexBanner").offset().top
+    }, 'slow');
+
+});
+// For smooth scrolling
+$("#forwardBtn").on('click', function (event) {
+
+    $('html, body').animate({
+        scrollTop: $("#navBar").offset().top
+    }, 'slow');
+
+});
+// For smooth scrolling
+$("#forwardBtn").on('click', function (event) {
+
+    $('html, body').animate({
+        scrollTop: $("#navBar").offset().top
+    }, 'slow');
+
+});
+// For smooth scrolling
+$("#forwardBtn").on('click', function (event) {
+
+    $('html, body').animate({
+        scrollTop: $("#navBar").offset().top
+    }, 'slow');
+
+});
